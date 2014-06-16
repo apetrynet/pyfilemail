8b1eef030425adeac3f8bcb52aabfaead2c2173b
Daniel Flehner Heen <flehnerheener@gmail.com>
2014-06-17 00:25:08 +0200

    added some info

--- filemail/__init__.py
+++ filemail/__init__.py
@@ -2,6 +2,13 @@
 from filemail import User, Transfer, Config
 
 
+__title__ = 'filemail'
+__version__ = '0.1.0'
+__author__ = 'Daniel Flehner Heen'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2014 Daniel Flehner Heen'
+
+
 def login(username, apikey=None, password=None, **kwargs):
     user = User(username, apikey, password, **kwargs)
     user.login()
