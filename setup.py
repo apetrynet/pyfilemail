cc86ae2b04849f1b582f6e8fdceea75b93d6e052
Daniel Flehner Heen <flehnerheener@gmail.com>
2016-06-03 23:12:15 +0200

    fixed faulty license classifier

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ file transfers with www.filemail.com',
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'Natural Language :: English',
-        'License :: OSI Approved :: MIT',
+        'License :: OSI Approved :: MIT License',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7'
