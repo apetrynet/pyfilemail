924c116937e41945aaf106840e5d2a17518d6bf0
Daniel Flehner Heen <flehnerheener@gmail.com>
2016-06-03 23:26:42 +0200

    Bump version: 0.1.0 → 0.2.0

--- pyfilemail/__init__.py
+++ pyfilemail/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 __title__ = 'pyfilemail'
-__version__ = '0.1.0'
+__version__ = '0.2.0'
 __author__ = 'Daniel Flehner Heen'
 __license__ = 'MIT'
 __copyright__ = 'Copyright 2016 Daniel Flehner Heen'
